6bc1b6d8d12f9a0c7921affee26a81c362b34835
cypher <satk124@gmail.com>
2017-01-04 01:24:38 +0530

    Driver formating

--- src/romanCalculator.java
+++ src/romanCalculator.java
@@ -11,23 +11,19 @@ public class romanCalculator {
 		System.out.println("***Roman Arithmetic*** ");
 		System.out.println("Allowed Operations [1->addition, 2->subtraction, 3->multiplication, 4->division]\nRULE\n1. Numeral must be in standard representation[e.g.,following subtractives are allowed IV & IX , XL & XC, CD & CM ]\n2. Following subtractives are not allowed [IL, IC, IM, XD, XM, LC, LD, LM] ");
 		System.out.println("3. Limit[latter's limit=[MDCLXVI], Max Roman numeral=MMMCMXCIX &  Min Roman numeral=I]");
-		System.out.println("INPUT Format\nnumeral1 numeral2 operation [e.g., XL X 1] ");
+		System.out.println("INPUT Format\nnumeral1 numeral2 operation [e.g., XL X 1] ");		
 		
 
-		//check()  we can "subtract" a symbol representing a IV & IX , XL & XC, CD & CM from the next two higher symbols,
-		//convert to proper format remove subtractive->compact[-> add subtractive]
-		//
-		boolean flag=true;
-
-		while(flag){
+		while(true){
 
 			boolean check=true;
 			try{
 				numeral1=new StringBuilder(sc.next().toUpperCase());
 				numeral2=new StringBuilder(sc.next().toUpperCase());
+				System.out.println(numeral1);
 				operation =sc.nextInt();
 			}catch(Exception e){
-				System.out.print("\nInput not valid: "+"\n");
+				System.out.print("\nInput not valid: "+e+"\n");
 				check=false;
 			}
 			if(check && !RomanMath.isValidNumeral(numeral1) || !RomanMath.isValidNumeral(numeral2)){
@@ -40,28 +36,32 @@ public class romanCalculator {
 			}
 
 			if(check){	
+				String output;
 				switch(operation){
 
 				case 1:
 					System.out.println(RomanMath.add(numeral1, numeral2));
 					break;
 				case 2:
-					if(RomanMath.subtraction(numeral1, numeral2).toString().equals("-"))
+					output=RomanMath.subtraction(numeral1, numeral2).toString();
+					if(output.equals("-"))
 						System.out.println("not possible");
 					else
-						System.out.println(RomanMath.subtraction(numeral1, numeral2));
+						System.out.println(output);
 					break;
 				case 3:			
-					if(RomanMath.multiplication(numeral1, numeral2).toString().equals("-"))
+					output=RomanMath.multiplication(numeral1, numeral2).toString();
+					if(output.equals("-"))
 						System.out.println("out of range");
 					else
-						System.out.println(RomanMath.multiplication(numeral1, numeral2));
+						System.out.println(output);
 					break;
 				case 4:
-					if(RomanMath.division(numeral1, numeral2).toString().equals("-"))
+					output=RomanMath.division(numeral1, numeral2).toString();
+					if(output.equals("-"))
 						System.out.println("not possible");
 					else
-						System.out.println(RomanMath.division(numeral1, numeral2));
+						System.out.println(output);
 					break;
 				default :
 				}
@@ -70,7 +70,7 @@ public class romanCalculator {
 			try{
 				if(!check)sc.next();
 				int con=sc.nextInt();
-				if(con!=1) flag=false;
+				if(con!=1) break;
 			}catch( InputMismatchException e ){
 				System.exit(0);
 			}
